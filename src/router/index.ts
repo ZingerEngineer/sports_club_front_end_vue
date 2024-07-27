b3a923bc00ac4f9825e6ee3748637ba6c1593e7d
ZingerEngineer <ramyisez@gmail.com>
2024-07-28 00:25:05 +0300

    Added new register route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterationView from '../views/RegisterationView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+
+  {
+    path: '/register',
+    name: 'register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: RegisterationView
   }
 ]
 
@@ -24,4 +34,3 @@ const router = createRouter({
 })
 
 export default router
-
