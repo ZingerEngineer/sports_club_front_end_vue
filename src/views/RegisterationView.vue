bb9a718c33cf1955af7cc4bb87371dfffb86d3f7
ZingerEngineer <ramyisez@gmail.com>
2024-07-28 00:25:23 +0300

    *WIP Registeration view

---
+++ src/views/RegisterationView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="registeration-wrapper flex flex-row items-center justify-center">
+    <form onsubmit="handleSubmit">
+      <InputComponent label="lolo" />
+      <ButtonComponent
+        title="lolo"
+        :icon="['fab', 'google']"
+      />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonComponent from '@/components/ButtonComponent.vue' // @ is an alias to /src
+import InputComponent from '@/components/InputComponent.vue'
+export default defineComponent({
+  name: 'RegisterationView',
+  components: {
+    ButtonComponent,
+    InputComponent
+  }
+})
+</script>
