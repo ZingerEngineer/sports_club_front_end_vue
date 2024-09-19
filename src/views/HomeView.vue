39ee03be9135b2180ac7d0c23858cacd879f0fb5
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-09-20 02:44:57 +0300

    Created home view.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="home">
-    <ButtonComponent
-      class=""
-      title="Sign in with google"
-      :icon="['fab', 'google']"
-    />
+  <div class="w-full h-full flex items-center justify-center">
+    <div class="welcome-card-wrapper bg-lime-500 h-96 w-full flex flex-row">
+      <div class="left-side-title w-2/6">
+        <h1 class="text-4xl text-white font-bold">
+          Welcome to our sports club!
+        </h1>
+        <p class="font-semibold text-white">Together we succeed</p>
+      </div>
+      <div class="w-4/6">
+        <img
+          class="right-side-image"
+          src="@/assets/landing-page-bg.jpg"
+          alt="landing-page-background"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import ButtonComponent from '@/components/ButtonComponent.vue' // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    ButtonComponent
-  }
-})
 </script>
+
+<style scoped>
+.landing-page-background {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
